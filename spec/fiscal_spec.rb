f5057b0f8356fcad5108a36786b15d93c9639be3
Eric Hansen <eric@fluxxlabs.com>
2016-03-31 17:11:56 -0700

    add some new tests

--- spec/fiscal_spec.rb
+++ spec/fiscal_spec.rb
@@ -46,4 +46,44 @@ describe Fiscal do
     Date.today.fiscal.year.start.should eql(Date.today.beginning_of_year)
   end
 
+  it "returns the beginning of a quarter for a particular date" do
+    Date.fiscal(date: '2014-05-10', mm: 4, dd: 6).quarter.start.should eql('2014-04-06'.to_date)
+  end
+
+  it "calculates the previous fiscal quarter" do
+    start_date = '2014-05-10'.to_date
+    previous_quarter = start_date - 3.month # subtract a quarter
+    Date.fiscal(date: previous_quarter, mm: 4, dd: 6).quarter.start.should eql('2014-01-06'.to_date)
+  end
+
+  it "calculates the next fiscal quarter" do
+    start_date = '2014-05-10'.to_date
+    previous_quarter = start_date + 3.month # add a quarter
+    Date.fiscal(date: previous_quarter, mm: 4, dd: 6).quarter.start.should eql('2014-07-06'.to_date)
+  end
+
+  it "calculates 10th previous fiscal quarter" do
+    start_date = '2014-05-10'.to_date
+    previous_quarter = start_date - 30.month # subtract 10 quarters
+    Date.fiscal(date: previous_quarter, mm: 4, dd: 6).quarter.start.should eql('2011-10-06'.to_date)
+  end
+
+  it "calculates the previous fiscal year" do
+    start_date = '2014-05-10'.to_date
+    previous_quarter = start_date - 1.year # subtract a year
+    Date.fiscal(date: previous_quarter, mm: 4, dd: 6).quarter.start.should eql('2013-04-06'.to_date)
+  end
+
+  it "calculates the next fiscal year" do
+    start_date = '2014-05-10'.to_date
+    previous_quarter = start_date + 1.year # add a year
+    Date.fiscal(date: previous_quarter, mm: 4, dd: 6).quarter.start.should eql('2015-04-06'.to_date)
+  end
+
+  it "calculates 10th previous fiscal year" do
+    start_date = '2014-05-10'.to_date
+    previous_quarter = start_date - 10.year # subtract 10 years
+    Date.fiscal(date: previous_quarter, mm: 4, dd: 6).quarter.start.should eql('2004-04-06'.to_date)
+  end
+
 end
